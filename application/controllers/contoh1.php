14e53c4bfbc54c0b05c78fd75a0555de6c761dee
hilmiadha <hilmiadha78@gmail.com>
2023-09-25 16:32:13 +0700

    tambah data

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo"<h1>Perkenalan<h1>";
+    echo"Nama saya Muhammad Zulhilmi Adha
+    Saya tinggal di daerah Magelang
+    Olah raga yang saya sukai adalah
+    Sepak bola";
+    }
+}
